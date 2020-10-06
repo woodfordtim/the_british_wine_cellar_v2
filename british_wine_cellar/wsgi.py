61b183ca9de0bd00a0a148f9d9f6488c3aab34ed
woodfordtim <woodfordtim@gmail.com>
2020-10-06 22:01:40 +0000

    Initial commit

---
+++ british_wine_cellar/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for british_wine_cellar project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'british_wine_cellar.settings')
+
+application = get_wsgi_application()
