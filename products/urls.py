3b5a28d4fc4110af9c2928944857f084dda85123
woodfordtim <woodfordtim@gmail.com>
2020-10-18 21:16:54 +0000

    Structure product.html page and add product details functionality

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_wines, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Wine
 
 
@@ -12,3 +12,15 @@ def all_wines(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def wine_details(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Wine, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
