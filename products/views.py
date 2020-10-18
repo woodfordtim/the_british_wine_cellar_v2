d5bd9c9120db2056e88670991bcac5e486f96b39
woodfordtim <woodfordtim@gmail.com>
2020-10-18 15:27:57 +0000

    Add products views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Wine
 
-# Create your views here.
+
+def all_wines(request):
+    """ A view to return all wines, including sorting and search queries """
+
+    products = Wine.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
