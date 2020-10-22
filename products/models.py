9028e03b56f052027d40581ec378b62839dea87d
woodfordtim <woodfordtim@gmail.com>
2020-10-22 20:40:46 +0000

    Fix product filter issue

--- products/models.py
+++ products/models.py
@@ -1,19 +1,9 @@
 from django.db import models
 
 
-class RegionManager(models.Manager):
-    def get_by_natural_key(self, friendly_name):
-        return self.get(friendly_name=friendly_name)
-
-
 class Region(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    objects = RegionManager()
-
-    class Meta:
-        unique_together = [['friendly_name']]
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Wine
+from .models import Wine, Wine_type, Region, Winery
 
 
 def all_wines(request):
@@ -9,12 +9,26 @@ def all_wines(request):
 
     products = Wine.objects.all()
     query = None
-    wine_type = None
+    wine_types = None
+    regions = None
+    wineries = None
 
     if request.GET:
         if 'wine_type' in request.GET:
             wine_types = request.GET['wine_type']
-            products = products.filter(wine_type__name__in=wine_types)
+            products = products.filter(wine_type__name=wine_types)
+            wine_types = Wine_type.objects.filter(name__in=wine_types)
+
+        if 'region' in request.GET:
+            regions = request.GET['region']
+            products = products.filter(region__name=regions)
+            regions = Region.objects.filter(name__in=regions)
+
+        if 'winery' in request.GET:
+            wineries = request.GET['winery']
+            products = products.filter(winery__name=wineries)
+            wineries = Winery.objects.filter(name__in=wineries)
+
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -25,12 +39,11 @@ def all_wines(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-
     context = {
         'products': products,
         'search_term': query,
     }
-    print(f"products Queryset: {products}")
+
     return render(request, 'products/products.html', context)
 
 
