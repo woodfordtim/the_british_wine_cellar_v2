b65135673e5d09342b14d8a808e973857a1f7c24
woodfordtim <woodfordtim@gmail.com>
2020-10-30 17:17:52 +0000

    Deploy to Heroku

--- british_wine_cellar/settings.py
+++ british_wine_cellar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 's6!#uwq!yuqy=fwq@oq8^*w7&%f+x)q3bhf+t(w@ygp87hpzfh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-british-wine-cellar.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,18 @@ WSGI_APPLICATION = 'british_wine_cellar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
     }
-}
+else:
+    DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
